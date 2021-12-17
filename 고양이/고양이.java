9afa787062569721880b60b26cd628b23ab72cee
SangwonYoon <yoonsw0532@naver.com>
2021-12-17 12:58:22 +0900

    21-12-17 java practice

---
+++ 고양이/고양이.java
@@ -0,0 +1,10 @@
+package 고양이;
+
+public class 고양이 {
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
